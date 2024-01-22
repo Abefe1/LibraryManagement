2842c053114fb1857d2aac01f272206e27032812
LUKMON Olanrewaju Akingbade <98253147+Abefe1@users.noreply.github.com>
2024-01-22 16:10:30 +0100

    Delete LibraryManagement/LibraryManagement directory

--- LibraryManagement/LibraryManagement/src/main/java/com/example/LibraryManagement/exceptions/BookUserNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.example.LibraryManagement.exceptions;
-
-public class BookUserNotFoundException extends RuntimeException{
-    public BookUserNotFoundException(String message) {
-        super(message);
-    }
-}
